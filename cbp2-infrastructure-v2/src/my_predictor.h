15107c9e2fc5518307b3f8912b88afddf33860bb
Eric <ericabeast@tamu.edu>
2022-11-06 13:53:17 -0600

    Add hello signature

--- cbp2-infrastructure-v2/src/my_predictor.h
+++ cbp2-infrastructure-v2/src/my_predictor.h
@@ -4,51 +4,65 @@
 // Note that this predictor doesn't use the whole 32 kilobytes available
 // for the CBP-2 contest; it is just an example.
 
-class my_update : public branch_update {
+class my_update : public branch_update
+{
 public:
 	unsigned int index;
 };
 
-class my_predictor : public branch_predictor {
+class my_predictor : public branch_predictor
+{
 public:
-#define HISTORY_LENGTH	15
-#define TABLE_BITS	15
+#define HISTORY_LENGTH 15
+#define TABLE_BITS 15
 	my_update u;
 	branch_info bi;
 	unsigned int history;
-	unsigned char tab[1<<TABLE_BITS];
+	unsigned char tab[1 << TABLE_BITS];
 
-	my_predictor (void) : history(0) { 
-		memset (tab, 0, sizeof (tab));
+	my_predictor(void) : history(0)
+	{
+		memset(tab, 0, sizeof(tab));
 	}
 
-	branch_update *predict (branch_info & b) {
+	branch_update *predict(branch_info &b)
+	{
 		bi = b;
-		if (b.br_flags & BR_CONDITIONAL) {
-			u.index = 
-				  (history << (TABLE_BITS - HISTORY_LENGTH)) 
-				^ (b.address & ((1<<TABLE_BITS)-1));
-			u.direction_prediction (tab[u.index] >> 1);
-		} else {
-			u.direction_prediction (true);
+		if (b.br_flags & BR_CONDITIONAL)
+		{
+			u.index =
+				(history << (TABLE_BITS - HISTORY_LENGTH)) ^ (b.address & ((1 << TABLE_BITS) - 1));
+			u.direction_prediction(tab[u.index] >> 1);
 		}
-		u.target_prediction (0);
+		else
+		{
+			u.direction_prediction(true);
+		}
+		u.target_prediction(0);
 		return &u;
 	}
 
-	void update (branch_update *u, bool taken, unsigned int target) {
-		if (bi.br_flags & BR_CONDITIONAL) {
-			unsigned char *c = &tab[((my_update*)u)->index];
-			if (taken) {
-				if (*c < 3) (*c)++;
-			} else {
-				if (*c > 0) (*c)--;
+	void update(branch_update *u, bool taken, unsigned int target)
+	{
+		if (bi.br_flags & BR_CONDITIONAL)
+		{
+			unsigned char *c = &tab[((my_update *)u)->index];
+			if (taken)
+			{
+				if (*c < 3)
+					(*c)++;
+			}
+			else
+			{
+				if (*c > 0)
+					(*c)--;
 			}
 			history <<= 1;
 			history |= taken;
-			history &= (1<<HISTORY_LENGTH)-1;
+			history &= (1 << HISTORY_LENGTH) - 1;
 		}
 	}
 };
 
-//Hallo Welt！ Haiming
+// Hallo Welt！ Haiming
+//  Hello Eirc
