4894d9b6fa7c87ec0f01de7e76632e0bdc3f9dce
TingweiSu <willytwsu@tamu.edu>
2022-12-05 20:43:51 -0600

    [Infra] Updated infra v3 by Prof Jimenez

---
+++ cbp2-infrastructure-v3/src/my_predictor.h
@@ -0,0 +1,59 @@
+// my_predictor.h
+// This file contains a sample my_predictor class.
+// It has a simple 32,768-entry gshare with a history length of 15 and a
+// simple direct-mapped branch target buffer for indirect branch prediction.
+
+class my_update : public branch_update {
+public:
+	unsigned int index;
+};
+
+class my_predictor : public branch_predictor {
+public:
+#define HISTORY_LENGTH	15
+#define TABLE_BITS	15
+	my_update u;
+	branch_info bi;
+	unsigned int history;
+	unsigned char tab[1<<TABLE_BITS];
+	unsigned int targets[1<<TABLE_BITS];
+
+	my_predictor (void) : history(0) { 
+		memset (tab, 0, sizeof (tab));
+		memset (targets, 0, sizeof (targets));
+	}
+
+	branch_update *predict (branch_info & b) {
+		bi = b;
+		if (b.br_flags & BR_CONDITIONAL) {
+			u.index = 
+				  (history << (TABLE_BITS - HISTORY_LENGTH)) 
+				^ (b.address & ((1<<TABLE_BITS)-1));
+			u.direction_prediction (tab[u.index] >> 1);
+		} else {
+			u.direction_prediction (true);
+		}
+		if (b.br_flags & BR_INDIRECT) {
+			u.target_prediction (targets[b.address & ((1<<TABLE_BITS)-1)]);
+			//u.target_prediction (0);
+		}
+		return &u;
+	}
+
+	void update (branch_update *u, bool taken, unsigned int target) {
+		if (bi.br_flags & BR_CONDITIONAL) {
+			unsigned char *c = &tab[((my_update*)u)->index];
+			if (taken) {
+				if (*c < 3) (*c)++;
+			} else {
+				if (*c > 0) (*c)--;
+			}
+			history <<= 1;
+			history |= taken;
+			history &= (1<<HISTORY_LENGTH)-1;
+		}
+		if (bi.br_flags & BR_INDIRECT) {
+			targets[bi.address & ((1<<TABLE_BITS)-1)] = target;
+		}
+	}
+};
